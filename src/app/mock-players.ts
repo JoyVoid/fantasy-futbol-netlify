fdf351316d12b7da83f00b6daf6bfdd5c3f35136
JoyVoid <joy.e.price@outlook.com>
2022-10-20 21:38:21 -0700

    edited dashboard

--- src/app/mock-players.ts
+++ src/app/mock-players.ts
@@ -1,14 +1,16 @@
+import { ɵisDefaultChangeDetectionStrategy } from '@angular/core';
+import { NEVER } from 'rxjs';
 import { Player } from './player';
 
 export const PLAYERS: Player[] = [
-    {id: 12, name: 'A.J. DeLaGarza'},
-    {id: 13, name: 'Aaron Herrera'},
-    {id: 14, name: 'Aaron Schoenfeld'},
-    {id: 15, name: 'Abdoulaye Cissoko'},
-    {id: 16, name: 'Aboubacar Keita'},
-    {id: 17, name: 'Abraham Rodriguez'},
-    {id: 18, name: 'Adam Armour'},
-    {id: 19, name: 'Aedan Stanley'},
-    {id: 20, name: 'Ahmed Hamdi'},
+    {id: 12, name: 'A.J. DeLaGarza', team: "NER", position: "D",salary: 250000},
+    {id: 13, name: 'Aaron Herrera',team: "RSL",position: "D",salary: 315000},
+    {id: 14, name: 'Aaron Schoenfeld',team:"ATX",position:"F",salary: 210000},
+    {id: 15, name: 'Abdoulaye Cissoko',team:"SEA",position:"D",salary: 63547},
+    {id: 16, name: 'Aboubacar Keita',team:"CLB",position:"D",salary: 110000},
+    {id: 17, name: 'Abraham Rodriguez',team:"COL",position:"GK",salary: 63547},
+    {id: 18, name: 'Adam Armour',team:"CLT",position:"D",salary:63547},
+    {id: 19, name: 'Aedan Stanley',team:"ATX",position:"D",salary: 63547},
+    {id: 20, name: 'Ahmed Hamdi',team:"MTL",position:"M",salary: 400000},
 
 ];
